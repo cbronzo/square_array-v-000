8477a458ce470d9457acaf058763dbd221e4210f
Catherine Bronzo <cbronzo@mac.com>
2018-06-10 20:37:10 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,10 @@
 def square_array(array)
   new_array = []
-  array.each do |number|
-  new_array << number**2
+  array.each do |elemet|
+  new_array << element ** 2
   end
+  new_array
 end
+
+
+    
